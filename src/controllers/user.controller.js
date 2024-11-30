c8cfce342b805aa0297493be14b05846d0af4e16
Suraj Bisht <surajsibi2022@gmail.com>
2024-11-30 20:21:33 +0530

    add aggregate pipeline for channel profile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -175,38 +175,225 @@ const refreshAccessToken = dbWrapper(async (req, res) => {
   if (!incomingRefreshToken) {
     throw new ApiError(401, "unauthorized request")
   }
- try {
-   const decodedToken = jwt.verify(
-     incomingRefreshToken,
-     process.env.REFRESH_TOKEN_SECRET
-   )
-   const user = await User.findById(decodedToken?._id)
-   if (!user) {
-     throw new ApiError(401, "Invalid refresh token")
-   }
- 
-   if (incomingRefreshToken !== user?.refreshToken) {
-     throw new ApiError(401, "Refresh token is already used or expire")
-   }
- 
-   const options = {
-     httpOnly: true,
-     secure: true
-   }
-   const { accessToken, newRefreshToken } = await generateAccessAndRefreshTokens(user._id)
-   return res.status(200
-     .cookie("accessToken", accessToken, options)
-     .cookie("refreshToken", newRefreshToken, options)
-     .json(
-       new ApiResponse(200, { accessToken, newRefreshToken },"Access token refresh successfully")
-     )
-   )
- } catch (error) {
-  throw new ApiError(401,error.message || "invalid refresh token")
- }
+  try {
+    const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    )
+    const user = await User.findById(decodedToken?._id)
+    if (!user) {
+      throw new ApiError(401, "Invalid refresh token")
+    }
+
+    if (incomingRefreshToken !== user?.refreshToken) {
+      throw new ApiError(401, "Refresh token is already used or expire")
+    }
+
+    const options = {
+      httpOnly: true,
+      secure: true
+    }
+    const { accessToken, newRefreshToken } = await generateAccessAndRefreshTokens(user._id)
+    return res.status(200
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", newRefreshToken, options)
+      .json(
+        new ApiResponse(200, { accessToken, newRefreshToken }, "Access token refresh successfully")
+      )
+    )
+  } catch (error) {
+    throw new ApiError(401, error.message || "invalid refresh token")
+  }
+
+})
+
+const changeCurrentPassword = dbWrapper(async (req, res) => {
+  const { oldPassword, newPassword } = req.body
+  const user = await User.findById(req.user._id)
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, "invalid password")
+  }
+
+  user.password = newPassword
+  await user.save({ validateBeforeSave: false })
+
+  return res.status(200)
+    .json(
+      new ApiResponse(200, {}, "Password changed successfully")
+    )
 
 })
 
 
+const getCurrentUser = dbWrapper(async (req, res) => {
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200, req.user, "current user fetched successfull"
+      )
+    )
+})
 
-export { registerUser, loginUser, logoutUser,refreshAccessToken }
+const updateAccountDetails = dbWrapper(async (req, res) => {
+  const { username, email } = req.body
+
+  if (!username || !email) {
+    throw new ApiError(400, "all fields are required")
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        fullname,
+        email
+      }
+    },
+    { new: true }
+  ).select("-password")
+
+
+  return res.status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"))
+
+})
+
+const updateUserAvatar = dbWrapper(async (req, res) => {
+  const avatarLocalPath = req.file?.path
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "avatar file is missing")
+  }
+  const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+  if (!avatar.url) {
+    throw new ApiError(400, "error while uploading on avtar")
+  }
+
+  const user = await User.findByIdAndUpdate(req.user?._id,
+    {
+      $set: {
+        avatar: avatar.url
+      }
+    },
+    { new: true }
+  ).select("-password")
+
+  if (!user) {
+    throw new ApiError(400, "cant update avatar in db")
+  }
+
+  return res.status(200)
+    .json(new ApiResponse(200, user, "Avatar updated successfully"))
+
+})
+const updateUserCoverImage = dbWrapper(async (req, res) => {
+  const coverImageLocalPath = req.file?.path
+  if (!coverImageLocalPath) {
+    throw new ApiError(400, "cover image file is missing")
+  }
+  const coverImage = await uploadOnCloudinary(avatarLocalPath)
+
+  if (!coverImage.url) {
+    throw new ApiError(400, "error while uploading on cover image")
+  }
+
+  const user = await User.findByIdAndUpdate(req.user?._id,
+    {
+      $set: {
+        coverImage: coverImage.url
+      }
+    },
+    { new: true }
+  ).select("-password")
+
+  if (!user) {
+    throw new ApiError(400, "cant update cover image in db")
+  }
+
+  return res.status(200)
+    .json(new ApiResponse(200, user, "cover image updated successfully"))
+
+})
+
+const getUserChannelProfile = dbWrapper(async(req,res)=>{
+  const {username} = req.params
+  if(!username?.trim()){
+    throw new ApiError(400,"username is missing")
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match:{
+        username:username?.toLowerCase()
+      }
+    },
+    {
+      $lookup:{
+        from:"subscriptions",
+        localField:"_id",
+        foreignField:"channel",
+        as:"subscribers"
+      }
+    },
+    {
+      $lookup:{
+        from:"subscriptions",
+        localField:"_id",
+        foreignField:"subscriber",
+        as:"subscribedTo"
+      }
+    },
+    {
+      $addFields:{
+        subscribersCount:{
+          $size:"$subscribers"
+          
+        },
+        channelsSubscribedToCount:{
+          $size:"$subscribedTo"
+        },
+        isSubscribe:{
+          $cond:{
+            if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+            then:true,
+            else:false
+          }
+        }
+      }
+    },
+    {
+      $project:{
+        fullname:1,
+        username:1,
+        subscribersCount:1,
+        channelsSubscribedToCount:1,
+        isSubscribe:1,
+        avatar:1,
+        coverImage:1,
+        email:1
+
+
+      }
+    }
+  ])
+  if(!channel?.length){
+    throw new ApiError(400,"channel does not exist ")
+  }
+
+  return return
+  .status(200)
+  .json(
+    new ApiResponse(200,channel[0],"user channel fetched successfully")
+  )
+})
+
+
+
+
+
+export {
+  registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage,getUserChannelProfile
+}
